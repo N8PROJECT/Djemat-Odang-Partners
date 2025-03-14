58e1ad22a9264fd5426f01ae87a370602832edb6
N8PROJECT <151629931+N8PROJECT@users.noreply.github.com>
2025-03-14 09:29:31 +0700

    add assets

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"], // Pastikan semua file di-scan
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
