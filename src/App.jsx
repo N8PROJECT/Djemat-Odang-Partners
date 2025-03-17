545cf8e5ae433a4f33a0c5547ece8b236cf49776
N8PROJECT <151629931+N8PROJECT@users.noreply.github.com>
2025-03-17 14:42:10 +0700

    Add Section Peoples

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Service from "./component/service";
+import Peoples from "./component/peoples";
 
 function App() {
   return (
     <>
       <div>
         <Service />
+        <Peoples />
       </div>
     </>
   )
